f2fcc70b41a5242223bd0240dfe9960926b95681
Gabriele A. Ron <developer@groncyber.com>
2023-12-25 08:40:56 -0600

    Created an algorithm template
    Signed-off-by: Gabriele A. Ron <developer@groncyber.com>

---
+++ templates/algorithm_template.py
@@ -0,0 +1,17 @@
+from collections.abc import Sequence
+from typing import Self
+
+from project_dilemma.interfaces import Algorithm, Rounds
+
+
+class AlgorithmTemplate(Algorithm):
+    algorithm_id = 'algorithm_template'
+
+    def __init__(self, mutations: Sequence[Self]):
+        super().__init__(mutations)
+
+    @staticmethod
+    def decide(rounds: Rounds) -> bool:
+        # Place algorithm here
+        # Return true for cooperation, and false for defection
+        return True
